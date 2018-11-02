3191041146503788314df76c30b860195b62f5b0
Thiago <thiagolavor86@gmail.com>
2018-11-02 18:05:06 -0300

    Cadastro de usuario

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,7 +3,10 @@ import './Footer.css'
 
 export default props => (
     <footer className="footer">
-        Rodape
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Thiago <span className="text-danger">Lavor</span></strong>
+        </span>
     </footer>
 
 )
--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,8 +1,13 @@
 import React from  'react'
+import {Link} from 'react-router-dom'
+import logo from '../../imgs/logo1.png'
 import './Logo.css'
 
 
-export default props => 
+export default props => (
     <aside className="logo"> 
-        Logo
+    <Link to="/" className="logo">
+        <img src={logo} alt="logo"/>
+    </Link>
     </aside>
+)
